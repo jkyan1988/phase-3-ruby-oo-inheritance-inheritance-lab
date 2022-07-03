a16558bc98807cb2a4cb93bf3f6444b06d7ab3a1
James Kyan <james.kyan88@gmail.com>
2022-07-03 12:08:31 -0400

    commit tree b4966d97605e619a3426216888d8227250bf1c9d

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
 
+    attr_writer :knowledge
+
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end
+    
+    def learn(string)
+        @knowledge.push(string)
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
